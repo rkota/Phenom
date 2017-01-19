4b97d801cc8eaffcb93b14bb833fb3ced2303e06
ramakrishnakota <ramakrishnak530@gmail.com>
2017-01-20 01:32:16 +0530

    initial commit2

--- src/main/scala/com/phenom/snippet/SnippetGen.scala
+++ src/main/scala/com/phenom/snippet/SnippetGen.scala
@@ -6,6 +6,10 @@ import scala.io.Source
 import play.libs.Json
 import com.fasterxml.jackson.databind.JsonNode
 import com.phenom.snippet.utils.Utils
+import edu.stanford.nlp.util.CollectionUtils
+import scala.collection.JavaConversions._
+import com.fasterxml.jackson.databind.ObjectMapper
+import play.api.libs.json.JsObject
 
 /**
  * @author ${user.name}
@@ -19,10 +23,20 @@ object SnippetGen {
     var inputFile = "./src/main/resources/jobs.json"
     
     for ( line <-Source.fromFile(inputFile).getLines){
+      var token_list = List[String]()
       var jnode = Json.parse(line)
-      jobs_post += jnode
       var desc_html = jnode.get("description").asText()
       jobs_desc += Utils.readtext(desc_html)
+      
+      for(text_line <- jobs_desc){
+      var l_words = Utils.lemmatize_words(text_line)
+      var list = CollectionUtils.getNGrams(l_words.toList, 1, 2)
+      var list_tokens = List(Set(list))
+      
+      jobs_post += jnode
+      println(list_tokens)
+      
+      }
     }
     
    /* for(text_line <- jobs_desc){
@@ -36,13 +50,10 @@ object SnippetGen {
       }
       }*/
     
-    for(text_line <- jobs_desc){
-      for(word <- Utils.lemmatize_words(text_line)){
-        println(word)
-        print("-----------------")
-      }
+    
         
     }
-    }
+    
+    
     
 }
